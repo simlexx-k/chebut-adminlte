85f060de429b98bddd95df776a2f062af83d7282
Simon Kosgei <simonwriter254@gmail.com>
2023-05-23 11:49:04 +0300

    modified:   admin/includes/header.php modified:   admin/includes/navbar.php

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -12,7 +12,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Title -->
-    <title>Chebut Tea FMS </title>
+    <title>Chebut Tea FMS - AdminLTE</title>
 
 
   	<!-- Tell the browser to be responsive to screen width -->
